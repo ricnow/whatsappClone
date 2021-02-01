5fbefb1e3f7637ba8b61bc9e7b0155e65da3d8ad
Richelle Soares <richelle.soares@gmail.com>
2021-02-01 17:47:42 -0300

    Backend

--- App.tsx
+++ App.tsx
@@ -1,17 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import Amplify from 'aws-amplify';
+import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify';
+import { getTodo } from './src/graphql/queries';
+import { createTodo } from './src/graphql/mutations';
 import config from './src/aws-exports';
 Amplify.configure(config)
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const getRand
+  //Rodas esse snippet apenas quando o App for montado a primeira vez
+  useEffect(async() => {
+    const fetchUser = async () => {
+      // Pegar a autenticação do usuário
+      const userInfo = await Auth.currentAuthenticatedUser({bypassCache: true })
+      console.log(userInfo);
+      if (userInfo) {
+
+         //Pegar o usuário pelo id
+        const userData = await API.graphql(graphqlOperation(getTodo, { id: userInfo.attributes.sub }))
+
+        if (userData.data.getUser) {
+          return;
+        }
+
+        const newUser = {
+          id: userInfo.attributes.sub,
+          name: ,
+          imageUri:  ,
+          status: 'Olá, estou usando o WhatsApp' ,
+        }
+      }
+      
+      
+
+      //Se não tiver um usuario no BD fazer a inscrição
+
+    }
+    fetchUser();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
